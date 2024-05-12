f88317d055f91295903a6f19fbf70a0e1521d032
gowthamka98 <gowtham.k@digicollect.com>
2024-05-12 15:19:58 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json())
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!')
 });
@@ -21,9 +25,11 @@ app.get('/', (req, res) => {
     res.send({
         message: 'Hello World!'
     })
+    // OR
     // res.json({
     //     message: 'Hello World!'
     // })
 })
 
 app.use("/api/user", userRouter)
+app.use('/api/auth', authRouter)
