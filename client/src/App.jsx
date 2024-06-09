cc7ba58cb15b67d99dd0bd5f5985ff5ef07e8b5b
gowthamka98 <gowtham.k@digicollect.com>
2024-06-09 11:27:46 +0530

    complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './Components/Header'
 import PrivateRoute from './Components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
  
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Route>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
